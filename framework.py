83f0e612642e3b1d85af6ee14a5a9cba77469c83
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-05-11 19:59:03 +0300

    hw

--- framework.py
+++ framework.py
@@ -4,14 +4,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 
-def wait_time(title, driver, timeout=3):
+def wait_time(title, driver, timeout=2):
     try:
         WebDriverWait(driver, timeout).until((EC.title_is(title)))
     except TimeoutException:
         raise AssertionError(f"Не было элемента{title}")
 
 
-def assert_element(selector, driver, timeout=1):
+def assert_element(selector, driver, timeout=2):
     try:
         WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
     except TimeoutException:
--- test_opencart.py
+++ test_opencart.py
@@ -1,3 +1,4 @@
+import pytest
 from framework import wait_time, assert_element
 
 
@@ -6,22 +7,24 @@ def test_title(browser):
 
 
 # 2.1
-# @pytest.mark.parametrize('selector', ['[href="http://localhost/desktops"]',
-#                                       '[href="http://localhost/component"]',
-#                                       '[href="http://localhost/tablet"]'],
-#                          '[href="http://localhost/software"]',
-#                          '[.fa.fa-search]'
-#                          )
-# def test_main_page(browser, selector):
-#     WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
-#
-#
-# # 2.2
-# @pytest.mark.parametrize('selector', ['']
-#                          )
-# def test_cataloge(browser):
-#     WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
-#
+@pytest.mark.parametrize('selector', ['[href="http://localhost/desktops"]',
+                                      '[href="http://localhost/component"]',
+                                      '[href="http://localhost/tablet"]',
+                         '[href="http://localhost/software"]',
+                         '.fa.fa-search']
+                         )
+def test_main_page(browser, selector):
+    wait_time("Your Store", browser)
+    assert_element(selector, browser)
+
+
+# 2.2
+@pytest.mark.parametrize('selector', ['']
+                         )
+def test_cataloge(browser, selector):
+    wait_time(wait_time("Your Store", browser))
+    assert_element(selector, browser)
+
 #
 # # 2.3
 # def test_cart_(browser):
