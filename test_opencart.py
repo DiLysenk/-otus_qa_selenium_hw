e2f857e48c7af6cb3c51489ec0348c8310e08252
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-05-12 17:49:58 +0300

    add hw

--- test_opencart.py
+++ test_opencart.py
@@ -10,44 +10,54 @@ def test_title(browser):
 @pytest.mark.parametrize('selector', ['[href="http://localhost/desktops"]',
                                       '[href="http://localhost/component"]',
                                       '[href="http://localhost/tablet"]',
-                         '[href="http://localhost/software"]',
-                         '.fa.fa-search']
+                                      '[href="http://localhost/software"]',
+                                      '.fa.fa-search']
                          )
 def test_main_page(browser, selector):
-    wait_time("Your Store", browser)
     assert_element(selector, browser)
 
 
 # 2.2
-@pytest.mark.parametrize('selector', ['']
+@pytest.mark.parametrize('selector', ['[for="input-sort"]',
+                                      '[for="input-limit"]',
+                                      '#compare-total',
+                                      '.breadcrumb',
+                                      '.img-responsive']
                          )
 def test_cataloge(browser, selector):
-    browser.get('')
-    wait_time("Your Store", browser)
-    assert_element(selector, browser)
+    browser.get('http://localhost/index.php?route=product/category&path=20')
+    assert_element(selector, browser, 3)
 
 
 # 2.3
-@pytest.mark.parametrize('selector', ['']
+@pytest.mark.parametrize('selector', ['.fa.fa-heart',
+                                      '.breadcrumb',
+                                      '[href="#tab-description"]',
+                                      '[href="#tab-review"]',
+                                      '.btn.btn-primary.btn-lg.btn-block']
                          )
 def test_cataloge(browser, selector):
-    browser.get('')
-    wait_time("Your Store", browser)
+    browser.get('http://localhost/index.php?route=product/product&path=57&product_id=49')
     assert_element(selector, browser)
 
-
 # 2.4
-@pytest.mark.parametrize('selector', ['']
+@pytest.mark.parametrize('selector', ['.breadcrumb',
+                                      '[type="submit"]',
+                                      '[value="Login"]',
+                                      '[placeholder="E-Mail Address"]',
+                                      '[placeholder="Password"]']
                          )
-def test_cataloge(browser, selector):
-    browser.get('')
-    wait_time("Your Store", browser)
+def test_login(browser, selector):
+    browser.get('http://localhost/index.php?route=account/login')
     assert_element(selector, browser)
 
 # 2.5
-@pytest.mark.parametrize('selector', ['']
+@pytest.mark.parametrize('selector', ['[placeholder="Username"]',
+                                      '[placeholder="Password"]',
+                                      '[href="http://localhost/admin/index.php?route=common/forgotten"]',
+                                      '[type="submit"]',
+                                      '[href="http://www.opencart.com"]']
                          )
 def test_cataloge(browser, selector):
-    browser.get('')
-    wait_time("Your Store", browser)
+    browser.get('http://localhost/admin/')
     assert_element(selector, browser)
