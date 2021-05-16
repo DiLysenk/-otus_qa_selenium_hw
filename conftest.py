b6709a350ce02d95cfe5cf32006f873ebd11729b
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-05-17 01:51:10 +0300

    hw

--- conftest.py
+++ conftest.py
@@ -1,8 +1,7 @@
 import pytest
 import os
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 DRIVERS = os.path.expanduser("~/Downloads/drivers")
 
